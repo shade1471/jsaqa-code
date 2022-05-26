ca6c053c7cc5603c719cf80dcc4574f95deaba44
Andrey Gribanov <shade1471@gmail.com>
2022-05-26 14:48:19 +0700

    Edit Task 7.3

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,24 @@
-const sorting = require("../../app");
+const sorting = require('../../app');
 
-describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
-  });
+describe('Books names test suit', () => {
+    it('Books names should be sorted in ascending order', () => {
+        expect(
+            sorting.sortByName([
+                'Гарри Поттер',
+                'Властелин Колец',
+                'Волшебник изумрудного города'
+            ])
+        ).toEqual([
+            'Властелин Колец',
+            'Волшебник изумрудного города',
+            'Гарри Поттер'
+        ]);
+    });
+    it('Books genres should be sorted in ascending order', () => {
+        expect(sorting.sortByName(['Фэнтези', 'Фэнтези', 'Сказка'])).toEqual([
+            'Сказка',
+            'Фэнтези',
+            'Фэнтези'
+        ]);
+    });
 });
